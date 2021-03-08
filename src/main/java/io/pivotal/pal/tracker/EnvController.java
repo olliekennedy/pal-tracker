c0a72cd8fe6a9bab123ea2248f51492af41c6da0
okenned3 <okenned3@ford.com>
2021-03-08 16:46:16 +0000

    The env route now shows information and pushing is less argument-needy

---
+++ src/main/java/io/pivotal/pal/tracker/EnvController.java
@@ -0,0 +1,33 @@
+package io.pivotal.pal.tracker;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class EnvController {
+
+    private final String port, memoryLimit, cfInstanceIndex, cfInstanceAddr;
+
+    public EnvController(@Value("${port:NOT SET}") String port,
+                         @Value("${memory.limit:NOT SET}") String memoryLimit,
+                         @Value("${cf.instance.index:NOT SET}") String cfInstanceIndex,
+                         @Value("${cf.instance.addr:NOT SET}") String cfInstanceAddr) {
+        this.port = port;
+        this.memoryLimit = memoryLimit;
+        this.cfInstanceIndex = cfInstanceIndex;
+        this.cfInstanceAddr = cfInstanceAddr;
+    }
+
+    @GetMapping("/env")
+    public Map<String, String> getEnv() {
+        return Map.of(
+                "PORT", this.port,
+                "MEMORY_LIMIT", this.memoryLimit,
+                "CF_INSTANCE_INDEX", this.cfInstanceIndex,
+                "CF_INSTANCE_ADDR", this.cfInstanceAddr
+        );
+    }
+}
